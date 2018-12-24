3e8a30d40a69e13b28d933f8388137780a759db4
mirae611 <mirae611@nate.com>
2018-12-24 19:39:00 +0900

    add Iscroll & Datepicker

--- js/admin.js
+++ js/admin.js
@@ -6,19 +6,19 @@ $(document).on('click', 'a[href="#"]', function(e) {
 
 $(document).ready(function() {
     //nav
-    $('header a.nav_btn').on('click', function() {
-        $('nav').toggleClass('open');
+    $('a.nav_btn').on('click', function() {
+        $('nav, main').toggleClass('off');
     });
 
     //nav menu
     navMenu();
     function navMenu() {
-        $('nav > ul > li').each(function() {
+        $('ul.nav_menu > li').each(function() {
             if($(this).find('li').length > 0) {
                 $(this).find('> a').append('<i class="fas fa-angle-down arrow"></i>');
             }
         });
-        $('nav > ul > li > a').on('click', function() {
+        $('ul.nav_menu > li > a').on('click', function() {
             if($(this).parent().hasClass('open')) {
                 $(this).parent().find('> ul').css({'height': 0});
                 $(this).parent().removeClass('open');
@@ -30,7 +30,33 @@ $(document).ready(function() {
                 $(this).parent().find('> ul').css({'height': height});
                 $(this).parent().addClass('open');
             }
-        });            
-    }
-});
+            setTimeout(function() {myScroll.refresh();}, 200);
+        });
+        
+        $('nav').on('mouseleave', function() {
+            $('ul.nav_menu > li:not(.on) > ul').css({'height': 0});
+            $('ul.nav_menu > li:not(.on)').removeClass('open');
+            setTimeout(function() {
+                myScroll.refresh();
+            }, 300);
+        });
+        
+    } // end of navMenu
+    
+}); // end of ready
 
+//IScroll적용
+var myScroll;
+
+function loaded () {
+    myScroll = new IScroll('#wrapper', {
+        scrollbars: true,
+        mouseWheel: true,
+        interactiveScrollbars: true,
+        shrinkScrollbars: 'scale',
+        fadeScrollbars: false
+    });
+    return myScroll;
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
